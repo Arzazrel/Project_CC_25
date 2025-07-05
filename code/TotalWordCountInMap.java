a74a34e435a48cedf386cb243545c856fbf6be4a
Arzazrel <88975224+Arzazrel@users.noreply.github.com>
2025-07-05 18:43:42 +0200

    added totalwordcount class with in-mapper combining

---
+++ code/TotalWordCountInMap.java
@@ -0,0 +1,248 @@
+package it.unipi.hadoop;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.Counters;
+import org.apache.hadoop.mapreduce.TaskCounter;
+
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+/**
+ * Class to implement a MapReduce total word count in the inputfiles, in-mapper combining version.
+ */
+public class TotalWordCountInMap
+{
+    /**
+     * Mapper class for implement the map logic for the total word count
+     */
+    public static class TotalWordCountMapper extends Mapper<Object, Text, Text, LongWritable>
+    {
+        private long localCount;                                    // local word counter
+        private static final Text word = new Text("word");          // unique key for all the words
+
+        // to initialize the data structures used fo in mapping
+        protected void setup(Context context) throws IOException, InterruptedException {
+            localCount = 0;  // inizializza il contatore
+        }
+
+        // map function
+        public void map(final Object key, final Text value, final Context context)
+                throws IOException, InterruptedException {
+            final StringTokenizer itr = new StringTokenizer(value.toString());  // split the given input text line into tokens
+            // iterate over each token obtained from the line
+            while (itr.hasMoreTokens()) {
+                itr.nextToken();
+                if (localCount < Long.MAX_VALUE)    // control check for overflow
+                {
+                    localCount++;       // update the counter
+                } else {
+                    flush(context);     // risk overflow, emit partial data
+                    localCount++;       // update the counter
+                }
+            }
+        }
+
+        // to close the data structures used fo in mapping
+        protected void cleanup(Context context) throws IOException, InterruptedException {
+            flush(context);         // emit and flush memory
+        }
+
+        // ---------------------- start: utility functions for in-combining ----------------------
+        // function to emit the data collected and flus the memory
+        private void flush(Context context) throws IOException, InterruptedException {
+            if (localCount > 0)     // control check
+            {
+                context.write(word, new LongWritable(localCount));  // emit
+                localCount = 0;                                     // reset counter
+            }
+        }
+        // ---------------------- end: utility functions for in-combining ----------------------
+    }
+
+    /**
+     * Reducer class to implement the reduce logic for the word count
+     */
+    public static class TotalWordCountReducer extends Reducer<Text, LongWritable, Text, LongWritable> {
+        private final LongWritable result = new LongWritable();   // var to set the value to associate with the found words
+
+        // reduce function
+        public void reduce(final Text key, final Iterable<LongWritable> values, final Context context)
+                throws IOException, InterruptedException {
+            long sum = 0;                           // var to the sum of the values associated to keys
+            for (final LongWritable val : values) {
+                sum += val.get();                   // update sum
+            }
+            result.set(sum);                // set the result with sum
+            context.write(key, result);     // emit
+        }
+    }
+
+    // ---------------------- start: utility functions ----------------------
+    /**
+     * function to format the time passed as parameter
+     *
+     * @param millis    time in milliseconds
+     * @return          the time in the correct format
+     */
+    public static String formatDuration(long millis) {
+        long hours = millis / (1000 * 60 * 60);
+        millis %= (1000 * 60 * 60);
+
+        long minutes = millis / (1000 * 60);
+        millis %= (1000 * 60);
+
+        long seconds = millis / 1000;
+        millis %= 1000;
+
+        return String.format("%02d:%02d:%02d.%03d", hours, minutes, seconds, millis);
+    }
+
+    /**
+     * function to retrieve the current date and time in the format "dd-MM-yyyy HH:mm:ss"
+     *
+     * @return  current date and time in this format "dd-MM-yyyy HH:mm:ss"
+     */
+    public static String getCurrentDateTime()
+    {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        return now.format(formatter);
+    }
+
+    // ---------------------- end: utility functions ----------------------
+
+    /**
+     * main function of the wordcount application.
+     * It repeats the same job multiple times and will collect metrics each time and then show an average of some,
+     * useful for testing. In the output data folder a different subfolder will be generated for each job run executed,
+     * at the end of the name there will be the number related to the job to distinguish the different outputs.
+     *
+     * @param args          <input path> <output base path> [numRuns]
+     * @throws Exception
+     */
+    public static void main(final String[] args) throws Exception {
+
+        String jobName = "TotalWordCountInMap";       // name for the job
+        // check the number of argument passed
+        if (args.length < 2) {
+            System.err.println("Usage: TotalWordCountInMap <input path> <output base path> [numRuns]");
+            System.exit(-1);
+        }
+        String inputPath = args[0];         // take the input folder
+        String outputBasePath = args[1];    // take base output folder
+
+        int numRunsRequested = 1;           // default value for the run
+        if (args.length >= 3)               // check if the user entered the third argument
+        {
+            numRunsRequested = Integer.parseInt(args[2]);   // take the number of runs to do
+        }
+        // var to manage the runs
+        int successfulRuns = 0;             // run successfully completed
+        long totalTime = 0;                 // total time used to perform all the runs of the job
+        int attempt = 0;                    // run's attempt
+
+        // while to perform the severals runs of the application
+        while (successfulRuns < numRunsRequested)
+        {
+            long startTime, endTime, duration;              // var to take the effective execution time
+
+            final Configuration conf = new Configuration(); // create configuration object
+            final Job job = new Job(conf, jobName + "_run_" + successfulRuns);
+            job.setJarByClass(TotalWordCountInMap.class);
+
+            job.setOutputKeyClass(Text.class);                  // set the typer for the output key for reducer
+            job.setOutputValueClass(LongWritable.class);        // set the typer for the output value for reducer
+
+            job.setMapperClass(TotalWordCountMapper.class);     // set mapper
+            job.setReducerClass(TotalWordCountReducer.class);   // set reducer
+
+            FileInputFormat.addInputPath(job, new Path(inputPath));     // first argument is the input folder
+
+            // Output folder specific for each successful run
+            String outputPath = outputBasePath + "/output_run_" + successfulRuns;   // set sub-path for the output
+            FileOutputFormat.setOutputPath(job, new Path(outputPath));              // give the output path to the job
+            System.out.println("\n--- Starting Job Attempt " + attempt + " ---");
+
+            startTime = System.currentTimeMillis();         // start time
+            boolean success = job.waitForCompletion(true);  // wait the end of the job
+            endTime = System.currentTimeMillis();           // end time
+
+            // take some statistics and visualize to console
+            if (success)    // if job ended with success
+            {
+                duration = endTime - startTime;             // time in milliseconds
+                totalTime += duration;                      // update total time
+                successfulRuns++;                           // update the number of the succesfully terminated job
+                System.out.println("Job " + successfulRuns + " completed successfully in " + formatDuration(duration));
+
+                // get some statistics
+                Counters counters = job.getCounters();
+                long mapInputRecords = counters.findCounter(TaskCounter.MAP_INPUT_RECORDS).getValue();
+                long mapOutputRecords = counters.findCounter(TaskCounter.MAP_OUTPUT_RECORDS).getValue();
+                long reduceInputRecords = counters.findCounter(TaskCounter.REDUCE_INPUT_RECORDS).getValue();
+                long reduceOutputRecords = counters.findCounter(TaskCounter.REDUCE_OUTPUT_RECORDS).getValue();
+                long spilledRecords = counters.findCounter(TaskCounter.SPILLED_RECORDS).getValue();
+
+                // print to console statistcs
+                System.out.println("=== Job Statistics ===");
+                System.out.println("Date: " + getCurrentDateTime());
+                System.out.println("Job Name: " + job.getJobName());
+                System.out.println("Job ID: " + job.getJobID());
+                System.out.println("Tracking URL: " + job.getTrackingURL());
+                System.out.println("Map Input Records: " + mapInputRecords);
+                System.out.println("Map Output Records: " + mapOutputRecords);
+                System.out.println("Reduce Input Records: " + reduceInputRecords);
+                System.out.println("Reduce Output Records: " + reduceOutputRecords);
+                System.out.println("Spilled Records: " + spilledRecords);
+                System.out.println("Application time: " + formatDuration(duration));
+
+                // write in a file txt -- see Note 0
+                try (PrintWriter writer = new PrintWriter(new FileWriter("job_stats.txt", true))) {
+                    writer.println("------------------------------------------");
+                    writer.println("=== Job Statistics ===");
+                    writer.println("Date: " + getCurrentDateTime());
+                    writer.println("Job Name: " + job.getJobName());
+                    writer.println("Job ID: " + job.getJobID());
+                    writer.println("Tracking URL: " + job.getTrackingURL());
+                    writer.println("Map Input Records: " + mapInputRecords);
+                    writer.println("Map Output Records: " + mapOutputRecords);
+                    writer.println("Reduce Input Records: " + reduceInputRecords);
+                    writer.println("Reduce Output Records: " + reduceOutputRecords);
+                    writer.println("Spilled Records: " + spilledRecords);
+                    writer.println("Application time: " + formatDuration(duration));
+                    writer.println("------------------------------------------");
+                }
+            }
+            else    // job failed
+            {
+                System.out.println("Job attempt " + attempt + " failed. Retrying...");
+            }
+            attempt++;      // update the attempt
+        }       // -- end - while --
+
+        double averageTime = totalTime / (double) successfulRuns;       // calculate the average execution time
+        System.out.println("\n=== All " + successfulRuns + " jobs completed successfully ===");
+        System.out.println("Average execution time: " + formatDuration((long)averageTime) + " ms");
+
+        System.exit(successfulRuns == numRunsRequested ? 0 : 1);   // exit, 0: all ok , 1: error
+    }
+}
+/*
+NOTE 0:
+    The job_stats.txt file is created in the same directory where you run the Java command (not in HDFS).
+    If the file already exists, the data is appended to the end (FileWriter with true for append mode).
+    The "Tracking URL" field shows you the address to monitor the job from the browser, useful if Hadoop is running in pseudo-distributed mode or on a real cluster.
+ */
