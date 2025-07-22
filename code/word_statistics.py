917753f567fadd1394e26d7f6df6545b82e14ca5
Arzazrel <88975224+Arzazrel@users.noreply.github.com>
2025-07-22 16:31:37 +0200

    Added new data results and modified the python program for dataset statistics

--- code/word_statistics.py
+++ code/word_statistics.py
@@ -10,12 +10,14 @@ def main():
 
     file_path = sys.argv[1]     # take the file 
     word_counts = {}            # dictionary for pair (word, occurrence)
+    tot_word_count = 0          # the total number of word
     
     with open(file_path, 'r') as file:          # read the file
         for line in file:                       # scroll the lines
             parts = line.strip().split("\t")    # split the line
             if len(parts) == 2:                 # check the numberof the split
                 word = parts[0]                 # take word
+                tot_word_count += 1             # update the counter
                 try:
                     count = int(parts[1])       # take occurrence
                     word_counts[word] = count   # put in the dictionay
@@ -23,6 +25,8 @@ def main():
                     pass                        # ignore malformed lines
 
     sorted_items = sorted(word_counts.items(), key=lambda x: x[1])  # sort by frequency
+
+    print("total number of word: ",tot_word_count)
 
     print("=== 10 least frequent words ===")
     for word, count in sorted_items[:10]:           # print the 10 least frequent words
